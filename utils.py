f60fd9b8216b1cd0278bfefe16219be7d771d654
Ankush Tale <37273059+ankushtale@users.noreply.github.com>
2019-04-09 16:30:29 +0530

    new function

--- utils.py
+++ utils.py
@@ -15,3 +15,19 @@ def listdir_full_path(d):
     return [os.path.join(d, f) for f in os.listdir(d)]
 
 
+def fetch_null_response_node(msg: str) -> dict:
+    response = dict()
+    in_disk = dict()
+    in_disk["page_count"] = None
+    in_disk["page_diff_flag"] = None
+    in_disk["annotations_list"] = None
+    in_disk["annotations_flag"] = None
+    in_disk["signature_present"] = None
+    in_disk["markup_present"] = None
+
+    response[msg] = in_disk
+
+    return response
+
+
+
